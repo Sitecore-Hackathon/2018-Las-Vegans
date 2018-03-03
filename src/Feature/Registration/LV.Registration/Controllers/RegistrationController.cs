7cf738eb3850b120a5a01a4db9869c9155ff99d9
wojciech-urban <wojciech.urban89@gmail.com>
2018-03-03 19:24:03 +0100

    controller registration body

--- src/Feature/Registration/LV.Registration/Controllers/RegistrationController.cs
+++ src/Feature/Registration/LV.Registration/Controllers/RegistrationController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LV.AirPolution.Services;
 
 namespace LV.Localization.Controllers
 {
     public class RegistrationController : Controller
     { 
         [HttpPost]
-        public ActionResult Register(string email, string lat, string lon)
+        public void Register(string email, string lat, string lon)
         {
-            return Json("ok");
+            var xConnectProvider = new XConnectProvider();
+
+            double latPar;
+            double lonPar;
+            
+            if(!double.TryParse(lat, out latPar))
+            {
+                throw new Exception("Can't parse lat parameter");
+            }
+
+            if (!double.TryParse(lon, out lonPar))
+            {
+                throw new Exception("Can't parse lon parameter");
+            }
+
+            xConnectProvider.RegisterUser(email, latPar, lonPar);
         }
     }
 }
