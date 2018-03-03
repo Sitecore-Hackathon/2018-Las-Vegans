49b28e2a9226628a7b973dfbc1d93ddf8de5fbfe
wojciech-urban <wojciech.urban89@gmail.com>
2018-03-03 11:29:18 +0100

    API service

---
+++ src/Foundation/AirPolution/code/Services/AirQualityService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using LV.AirPolution.Models;
+
+namespace LV.AirPolution.Services
+{
+    public class AirQualityService : IAirQualityService
+    {
+        public async Task<AirQualityResponse> GetAirQuality(AirQualityRequest request)
+        {
+            var url = GetUrl("baqi");
+            url = AddParameter(url, "lat", request.Lat.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture));
+            url = AddParameter(url, "lon", request.Lon.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture));
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = client.SendAsync(new HttpRequestMessage(HttpMethod.Get, url)).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                MemoryStream ms = new MemoryStream(Encoding.Unicode.GetBytes(responseString));
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(AirQualityResponse));
+                var result = serializer.ReadObject(ms) as AirQualityResponse;
+
+                return result;
+            }
+
+            throw new Exception($"Response with code: {response.StatusCode}, message: {response.ReasonPhrase}");
+        }
+
+        private string GetUrl(string methodName)
+        {
+            var apiUrl = ConfigurationManager.AppSettings["BreezoMetterApiUrl"];
+            var apiKey = ConfigurationManager.AppSettings["BreezoMetterApiKey"];
+
+            var url = $"{apiUrl}/{methodName}";
+            url = AddParameter(url, "key", apiKey);
+
+            return url;
+        }
+
+        private string AddParameter(string url, string name, string value)
+        {
+            var uriBuilder = new UriBuilder(url);
+            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            query[name] = value;
+            uriBuilder.Query = query.ToString();
+            url = uriBuilder.ToString();
+
+            return url;
+        }
+    }
+}
