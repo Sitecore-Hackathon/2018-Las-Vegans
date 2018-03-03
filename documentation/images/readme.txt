202a68cbb07680810268f946093d2a53d48e44f0
Tomasz Juranek <tjuranek@coders.center>
2018-03-03 07:06:02 +0100

    Initial solution structure for VS2017

---
+++ documentation/images/readme.txt
@@ -0,0 +1,1 @@
+put here screenshots for github help
---
+++ src/Foundation/AirPolution/tests/Tests/SampleTest.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using System.Collections.Generic;
+using Xunit;
+
+namespace SmartSitecore.CssPickerField.Tests.Cache
+{
+    public class SampleTest
+    {
+        [Fact]
+        public void TestMe()
+        {
+            
+        }
+    }
+}
