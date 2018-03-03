af7e37daef0e05004342766769f998792f3e7870
SOFTSERVE\rdebow <rdebow@softserveinc.com>
2018-03-03 17:07:32 +0100

    xconnect tests

--- src/Feature/Registration/LV.Registration/Controllers/RegistrationController.cs
+++ src/Feature/Registration/LV.Registration/Controllers/RegistrationController.cs
@@ -1,16 +1,25 @@
-﻿using System;
+﻿using LV.AirPolution.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace LV.Localization.Controllers
+namespace LV.Registration.Controllers
 {
     public class RegistrationController : Controller
     { 
         [HttpPost]
         public ActionResult Register(string email, string lat, string lon)
         {
+            return Json("");
+        }
+
+        [HttpGet]
+        public ActionResult XConnectTest()
+        {
+            XConnectProvider xConnectProvider = new XConnectProvider();
+            xConnectProvider.TestXConntect();
 
             return Json("");
         }
