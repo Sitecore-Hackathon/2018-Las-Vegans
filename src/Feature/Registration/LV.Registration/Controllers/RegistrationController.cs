840ea6ba52566568a60e2c31ff17132e9bda57a4
wojciech-urban <wojciech.urban89@gmail.com>
2018-03-03 16:22:13 +0100

    controller

---
+++ src/Feature/Registration/LV.Registration/Controllers/RegistrationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LV.Localization.Controllers
+{
+    public class RegistrationController : Controller
+    { 
+        [HttpPost]
+        public ActionResult Register(string email, string lat, string lon)
+        {
+
+            return Json("");
+        }
+    }
+}
